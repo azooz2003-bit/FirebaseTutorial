700dac4322ac64eba05c9d95961815d7c1722391
incordios <incordios@icloud.com>
2022-09-17 10:45:49 -0400

    first commit

---
+++ swiftui-tutorial/swiftui-tutorial/swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialApp.swift
+//  swiftui-tutorial
+//
+//  Created by Nicolas Rios on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
